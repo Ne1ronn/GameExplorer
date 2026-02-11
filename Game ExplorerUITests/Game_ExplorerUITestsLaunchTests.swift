47770d05bf192d785330f9e3a9b02921a9c05ef7
Yesset Kozhakhmet <esetkozcahmet87@gmail.com>
2026-02-11 23:29:02 +0500

    Initial Commit

---
+++ Game ExplorerUITests/Game_ExplorerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Game_ExplorerUITestsLaunchTests.swift
+//  Game ExplorerUITests
+//
+//  Created by Yesset on 11.02.2026.
+//
+
+import XCTest
+
+final class Game_ExplorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
